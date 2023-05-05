1d6d6d3b81e04a56f60fc250695d397dc4cd60fc
RPayne5715 <108432214+RPayne-5715@users.noreply.github.com>
2023-05-05 11:04:36 -0400

    tried to fix lack of background color

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function dropdown() {
     var x = document.getElementById("NavbarPages");
-    var y = document.getElementById("Navbar")
     if (x.className === "navbarPages") {
       x.className += " responsive";
     } else {
