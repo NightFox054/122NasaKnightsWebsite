48b38c6c2a41dba25422800e7a4bea27a7389ea2
RPayne5715 <108432214+RPayne-5715@users.noreply.github.com>
2023-05-05 09:45:26 -0400

    made gradients responsive

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function dropdown() {
-    var x = document.getElementById("Navbar");
+    var x = document.getElementById("NavbarPages");
+    var y = document.getElementById("Navber")
     if (x.className === "navbarPages") {
       x.className += " responsive";
     } else {
       x.className = "navbarPages";
     }
+
+    if (y.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+
   }
