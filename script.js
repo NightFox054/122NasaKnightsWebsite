2f9b7268f1c2d0d6930186f119f6d8985f46a961
RPayne5715 <108432214+RPayne-5715@users.noreply.github.com>
2023-05-07 21:16:37 -0400

    made animations non-replayable, i think

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ function reveal() {
     var elementVisible = -20;
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
+    //} else {
+    //  reveals[i].classList.remove("active");
+    //}
   }
 }
 
