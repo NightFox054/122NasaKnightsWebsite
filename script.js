3662dcc70d76ebeb8e8eddf950a6795ff675014b
RPayne5715 <108432214+RPayne-5715@users.noreply.github.com>
2023-05-09 09:35:32 -0400

    testing responsivity effort on navbar

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ function dropdown() {
 
   }
 
+var navbarShift = "-50px"
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset; //somehow this redundancy is necessary
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("NavContainer").style.top = "0";
   } else {
-    document.getElementById("NavContainer").style.top = "-50px";
+    document.getElementById("NavContainer").style.top = navbarShift;
   }
   prevScrollpos = currentScrollPos;
 }
